4d4052168a009f10082b4631a09fb752f8e21677
Olan Cruz Roberto Carlos <roberto7s@hotmail.com>
2026-06-07 23:04:58 +0000

    try to chnage icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit as st
+salesIcon = "/workspaces/OnionDataset/public/assets/sales-report.svg"
+
+st.set_page_config(page_title="Ventas Total", page_icon=salesIcon)
 
 st.switch_page("Venta Total.py")
